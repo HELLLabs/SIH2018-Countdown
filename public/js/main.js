8f84c8e66eed78e18fe665865b585cf129c7d584
Shubham Srivastava <shubham9918sri@gmail.com>
2018-03-29 14:47:32 +0530

    done

--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,29 @@
 var SCHEDULE = [
-    // [new Date('Fri Mar 30 2018 8:15:00'), 'Hackathon commencing soon'],
-    [new Date('Fri Mar 30 2018 8:30:00'), 'Hacking begins'],
-    [new Date('Fri Mar 30 2018 10:10:00'), 'Tea & snacks'],
+    [new Date('Fri Mar 30 2018 08:45:00'), 'Hacking begins'],
+    [new Date('Fri Mar 30 2018 10:00:00'), 'Tea & snacks'],
     [new Date('Fri Mar 30 2018 11:30:00'), 'First Round of Training session'],
     [new Date('Fri Mar 30 2018 13:00:00'), 'Lunch'],
-    [new Date('Fri Mar 30 2018 14:00:00'), 'Hacking Time'],
+    [new Date('Fri Mar 30 2018 14:00:00'), 'Hacking resume'],
     [new Date('Fri Mar 30 2018 16:30:00'), 'Tea & snacks'],
-    [new Date('Fri Mar 30 2018 17:00:00'), 'Hack Time'],
-    [new Date('Fri Mar 30 2018 19:00:00'), 'First round of evaluation'],
+    [new Date('Fri Mar 30 2018 17:00:00'), 'Hacking resume'],
+    [new Date('Fri Mar 30 2018 18:00:00'), 'First Round of Evaluation'],
+    [new Date('Fri Mar 30 2018 19:30:00'), 'Hacking resume'],
+    [new Date('Fri Mar 30 2018 20:00:00'), 'Address by hon. P.M.'],
     [new Date('Fri Mar 30 2018 20:30:00'), 'Dinner'],
-    [new Date('Fri Mar 30 2018 21:00:00'), 'Hack Time'],
-    [new Date('Fri Mar 30 2018 23:00:00'), 'Mid night energy drink'],
-    [new Date('Sat Mar 31 2018 00:00:00'), 'Second round of training'],
-    [new Date('Sat Mar 31 2018 1:00:00'), 'Recreational Activity'],
-    [new Date('Sat Mar 31 2018 1:30:00'), 'Hacking Time'],
-    [new Date('Sat Mar 31 2018 3:00:00'), 'Snacks Time'],
-    [new Date('Sat Mar 31 2018 6:00:00'), 'Yoga session'],
-    [new Date('Sat Mar 31 2018 6:30:00'), 'Breakfast Time'],
-    [new Date('Sat Mar 31 2018 9:00:00'), 'Second round of evaluation'],
-    [new Date('Sat Mar 31 2018 10:00:00'), 'Tea and snacks'],
-    [new Date('Sat Mar 31 2018 12:00:00'), 'Power mentoring'],
-    [new Date('Sat Mar 31 2018 12:45:00'), 'Lunch'],
-    [new Date('Sat Mar 31 2018 13:30:00'), 'Hacking time'],
-    [new Date('Sat Mar 31 2018 16:30:00'), 'tea and snacks'],
-    [new Date('Sat Mar 31 2018 18:00:00'), 'Hackathon concluding']
-]
+    [new Date('Fri Mar 30 2018 21:30:00'), 'Hacking resume'],
+    [new Date('Fri Mar 31 2018 00:00:00'), 'Second Round of Training session'],
+    [new Date('Fri Mar 31 2018 01:00:00'), 'Activity'],
+    [new Date('Fri Mar 31 2018 01:30:00'), 'Hacking resume'],
+    [new Date('Fri Mar 31 2018 06:00:00'), 'Yoga'],
+    [new Date('Fri Mar 31 2018 06:30:00'), 'Break fast'],
+    [new Date('Fri Mar 31 2018 07:30:00'), 'Hacking resume'],
+    [new Date('Fri Mar 31 2018 09:00:00'), 'Second Round of Evaluation'],
+    [new Date('Fri Mar 31 2018 10:30:00'), 'Hacking resume'],
+    [new Date('Fri Mar 31 2018 12:00:00'), 'Power monitoring'],
+    [new Date('Fri Mar 31 2018 12:45:00'), 'Lunch'],
+    [new Date('Fri Mar 31 2018 13:30:00'), 'Hacking resume'],
+    [new Date('Sat Mar 31 2018 15:00:00'), 'Hackathon concluding']
+];
 
 var ptime = pTime.getTime();
 var mainTiming = document.getElementById('main-timing');
@@ -52,7 +51,6 @@ function timer() {
     if (min < 10) {
         min = "0" + min;
     }
-    // console.log(hr, min)
     var date = cDate.toDateString().split(' ');
     currentTime.innerHTML = `${hr}:${min} ${amPM}`;
     currentDate.innerHTML = `${date[1]} ${date[2]}, ${date[3]}`;
@@ -63,7 +61,6 @@ function timer() {
         var mints = parseInt(netTime / (1000 * 60)) - ( hours * 60 );
         var sec = parseInt(netTime / (1000 )) - ( hours * 60 * 60 ) - ( mints * 60 );
         mainTiming.innerHTML = `${(hours < 10) ? '0' + hours : hours}:${(mints < 10) ? '0' + mints : mints}:${(sec < 10) ? '0' + sec : sec}`;
-        // mainTiming.innerHTML  = `${hours}:${mints}:${sec}`;
         ptime = ptime + 1000;
         introText.innerHTML='commencing '
     }
@@ -79,7 +76,6 @@ function timer() {
         var mints = parseInt(netTime / (1000 * 60)) - ( hours * 60 );
         var sec = parseInt(netTime / (1000 )) - ( hours * 60 * 60 ) - ( mints * 60 );
         mainTiming.innerHTML = `${(hours < 10) ? '0' + hours : hours}:${(mints < 10) ? '0' + mints : mints}:${(sec < 10) ? '0' + sec : sec}`;
-        // mainTiming.innerHTML  = `${hours}:${mints}:${sec}`;
         ptime = ptime + 1000;
         introText.innerHTML='concluding '
     }
@@ -88,17 +84,24 @@ function timer() {
 var i=0;
 var notfs = 0;
 function manageEvent() {
-    if (ptime < START.getTime() - 1*60*1000) {
+    if (ptime <= START.getTime() - 10*60*1000) {
         eventName.innerHTML = 'Hackathon commencing soon';
         timeLeft.innerHTML = ``;
         timeUnits.innerHTML = '';
         timeUnits.innerHTML = '';
         alert.innerHTML = '';
     }
+    else if(ptime > END.getTime())
+    {
+        eventName.innerHTML = 'Hackathon concludedes';
+        timeLeft.innerHTML = '';
+        timeUnits.innerHTML = '';
+        timeUnits.innerHTML = '';
+        alert.innerHTML = '';
+    }
     else{
         if (i < SCHEDULE.length) {
-            // console.log(parseInt((SCHEDULE[i+1][0].getTime()-ptime)/(1000 * 60)));
-            if ((SCHEDULE[i + 1][0].getTime() - ptime) < 10 * 60 * 1000) {
+            if(((SCHEDULE[i + 1][0].getTime() - ptime) < 10 * 60 * 1000)) {
                 i++;
             }
             if (((SCHEDULE[i][0].getTime() - ptime) >= 0) && ((SCHEDULE[i][0].getTime() - ptime) <= 10 * 60 * 1000)) {
@@ -120,14 +123,14 @@ function manageEvent() {
                     document.getElementById('foot').style.background = '#76B042';
                     notfs = 0
                 }
-                // eventName.innerHTML = 'Hackathon commencing soon!';
                 eventName.innerHTML = SCHEDULE[i][1];
-                timeLeft.innerHTML = ``;
+                timeLeft.innerHTML = '';
                 timeUnits.innerHTML = '';
                 timeUnits.innerHTML = '';
                 alert.innerHTML = '';
             }
         }
+
     }
 
 }
